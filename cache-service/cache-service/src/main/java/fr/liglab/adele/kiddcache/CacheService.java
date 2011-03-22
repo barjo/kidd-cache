9b4e6b1534fbcf74d484bac3d304d56aacbcc41c
barjo <astrosus@gmail.com>
2011-03-22 11:16:36 +0100

    Enhance CacheService javadoc and implements ExpirationDate.

--- cache-service/cache-service/src/main/java/fr/liglab/adele/kiddcache/CacheService.java
+++ cache-service/cache-service/src/main/java/fr/liglab/adele/kiddcache/CacheService.java
@@ -6,7 +6,7 @@ import java.util.Set;
 
 /**
  * The CacheService offers an easy way for caching commonly using data. 
- * TODO finish javadoc.
+ * TODO Reread and correct the javadoc.
  * @author barjo
  */
 public interface CacheService {
@@ -31,11 +31,11 @@ public interface CacheService {
 	
 	/**
 	 * Put a new value in the cache, identified by key which expire at expiration.
-	 * The cached policy is set to PutPolicy.ALWAYS.
+	 * The cached policy is set to {@link PutPolicy#ALWAYS}.
 	 * 
 	 * @param key The key for the new cache entry
 	 * @param value The data to be cached
-	 * @param expiration The {@code ExpirationDate} of the cached entry or {@code null} for none.
+	 * @param expiration The {@link ExpirationDate} of the cached entry or {@code null} for none.
 	 */
 	void put(Object key, Object value,ExpirationDate expiration);
 	
@@ -45,30 +45,69 @@ public interface CacheService {
 	 * 
 	 * @param key The key for the new cache entry
 	 * @param value The data to be cached
-	 * @param expiration The {@code ExpirationDate} of the cached entry or {@code null} for none.
-	 * @param policy {@code PutPolicy} storing strategy regarding pre-existing entries under the same key. 
+	 * @param expiration The {@link ExpirationDate} of the cached entry or {@code null} for none.
+	 * @param policy {@link PutPolicy} storing strategy regarding pre-existing entries under the same key. 
 	 * @return {@code true} if a new entry was created, {@code false} if not.
 	 */
 	boolean put(Object key, Object value,ExpirationDate expiration,PutPolicy policy);
 	
 	/**
-	 * Put 
-	 * @param values
+	 * Equivalent to {@link CacheService#putAll(Map, ExpirationDate, PutPolicy) putAll(values,null,PutPolicy.ALWAYS)}. 
+	 * @param entries (key/value) to be cached
 	 */
 	void putAll(Map<?,?> values);
 	
+	/**
+	 * Equivalent to {@link CacheService#putAll(Map, ExpirationDate, PutPolicy) putAll(values,expiration,PutPolicy.ALWAYS)}. 
+     *
+	 * @param values entries (key/value) to be cached
+	 * @param expiration expiration The {@link ExpirationDate} of the cached entry or {@code null} for none.
+	 */
 	void putAll(Map<?,?> values,ExpirationDate expiration);
 	
+	/**
+	 * Variant of {@link CacheService#put(Object, Object, ExpirationDate, PutPolicy)}.
+	 * 
+	 * @param values values entries (key/value) to be cached
+	 * @param expiration expiration expiration The {@link ExpirationDate} of the cached entry or {@code null} for none.
+	 * @param policy {@link PutPolicy} storing strategy regarding pre-existing entries under the same key. 
+	 * @return the set of keys for which entries were created. Keys in <code>values</code> may not be in the returned set because of the policy regarding pre-existing entries.
+	 */
 	<T> Set<T> putAll(Map<T,?> values,ExpirationDate expiration,PutPolicy policiy);
 	
+	/**
+	 * Fetch a cache value or null if unset.
+	 * @param key used to store the cache entry.
+	 * @return the value previously cached or <code>null</code> 
+	 */
 	Object get(Object key);
 	
-	<T> Map<T, Object> getAll(Collection<T> arg0);
+	/**
+	 * Multiple keys variant of {@link CacheService#get(Object)}.
+	 * @param keys used to store the cache entries 
+	 * @return A mapping of key to value of any cached entries found.
+	 */
+	<T> Map<T, Object> getAll(Collection<T> keys);
 	
+	/**
+	 * Test if there is a cache entry with key.
+	 * @param key used to store the cache entry
+	 * @return <code>true</code> if the cache contains an entry for the key.
+	 */
 	boolean contains(Object key);
 	
+	/**
+	 * Remove <code>key</code> from he cache.
+	 * @param key to be removed.
+	 * @return <code>true</code> if the entry existed and has been removed.
+	 */
 	boolean delete(Object key);
 	
+	/**
+	 * Multiple keys variant of {@link CacheService#delete(Object)}.
+	 * @param keys to be removed.
+	 * @return The keys found in the cache that have been deleted through this call.
+	 */
 	<T> Set<T> deleteAll(Collection<T> keys);
 	
 	
--- cache-service/cache-service/src/main/java/fr/liglab/adele/kiddcache/ExpirationDate.java
+++ cache-service/cache-service/src/main/java/fr/liglab/adele/kiddcache/ExpirationDate.java
@@ -1,9 +1,41 @@
 package fr.liglab.adele.kiddcache;
 
+import static java.lang.System.currentTimeMillis;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+import java.util.Date;
+
 /**
- * TODO
+ * TODO javadoc
  * @author barjo
  */
 public final class ExpirationDate {
+	private final Date date;
 	
+	private ExpirationDate(Date pDate){
+		date = pDate;
+	}
+	
+	long getDateInMilliseconds(){
+		return date.getTime();
+	}
+	
+	long getDateInSeconds(){
+		return SECONDS.convert(getDateInMilliseconds(), MILLISECONDS);
+	}
+	
+	public static ExpirationDate createFromDate(Date date){
+		return new ExpirationDate(date);
+	}
+	
+	public static ExpirationDate createFromDeltaMillis(int milliseconds){
+		Date edate = new Date(currentTimeMillis() + milliseconds);
+		return new ExpirationDate(edate);
+	}
+	
+	public static ExpirationDate createFromDeltaSeconds(int seconds){
+		Date edate = new Date(currentTimeMillis() + MILLISECONDS.convert(seconds, SECONDS));
+		return new ExpirationDate(edate);
+	}
 }
