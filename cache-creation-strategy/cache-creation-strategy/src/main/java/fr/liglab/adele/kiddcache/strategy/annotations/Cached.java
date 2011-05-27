474cbf5bad1428ae37b4ec7507747408c5bc9231
Bartek <Bartek@129.88.254.230>
2011-05-27 16:35:24 +0200

    annotations file fixed

---
+++ cache-creation-strategy/cache-creation-strategy/src/main/java/fr/liglab/adele/kiddcache/strategy/annotations/Cached.java
@@ -0,0 +1,14 @@
+package fr.liglab.adele.kiddcache.strategy.annotations;
+
+import java.lang.annotation.*;
+
+import fr.liglab.adele.kiddcache.CacheService;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Inherited
+public @interface Cached {
+
+	CacheService.PutPolicy policy() default CacheService.PutPolicy.ALWAYS;
+	int expireSeconds() default 0;
+}
